99fee13f2670d9aa721d109a1ea7dacae13ce9fd
gaopiji <448030133@qq.com>
2017-04-03 15:10:20 +0800

    完成场景搭建，坦克放置及前后控制

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+	public float speed = 5.0f;
+
+	private Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = this.GetComponent<Rigidbody> ();
+	}
+		
+	void FixedUpdate(){
+		//float h = Input.GetAxis ("Horizontal");
+		float h = Input.GetAxis ("Vertical");
+		rigidbody.velocity = transform.forward*h*speed;
+	}
+}
